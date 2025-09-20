7b6a8584753da69ae22d4ebe3ee9fad05ddb569e
Rafaelgayace <emanueldias35227@gmail.com>
2025-09-20 18:18:53 -0300

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -3,31 +3,36 @@ const fetch = require("node-fetch");
 const fs = require("fs-extra");
 const app = express();
 
-const CLIENT_ID = "1413933552533241990";
-const CLIENT_SECRET = "ST7euR0805MzA_za8eimv_hXXiIyt_Z_";
-const REDIRECT_URI = "https://imperialist-1.onrender.com/callback";
+// -------------------
+// Configurações do Discord
+// -------------------
+const OAUTH_URL = "https://discord.com/api/oauth2/authorize?client_id=1413933552533241990&redirect_uri=https%3A%2F%2Fimperialist-1.onrender.com%2Fcallback&response_type=code&scope=identify%20guilds%20connections%20guilds%20guilds.members.read";
 
-// URL de autorização completa
-const OAUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${1413933552533241990}&redirect_uri=${encodeURIComponent(https://imperialist-1.onrender.com/callback)}&response_type=code&scope=identify%20guilds%20connections%20guilds%20guilds.members.read`;
-
+// -------------------
 // Rota para iniciar login
+// -------------------
 app.get("/oauth2/authorize", (req, res) => {
   res.redirect(OAUTH_URL);
 });
 
+// -------------------
 // Rota callback do Discord
+// -------------------
 app.get("/callback", async (req, res) => {
   const code = req.query.code;
   console.log("Código recebido:", code);
 
   if (!code) return res.send("Código não encontrado.");
 
+  // -------------------
+  // Dados para trocar code por access token
+  // -------------------
   const data = new URLSearchParams();
-  data.append("client_id", 1413933552533241990);
-  data.append("client_secret", ST7euR0805MzA_za8eimv_hXXiIyt_Z_);
+  data.append("client_id", "1413933552533241990");
+  data.append("client_secret", "ST7euR0805MzA_za8eimv_hXXiIyt_Z_");
   data.append("grant_type", "authorization_code");
   data.append("code", code);
-  data.append("redirect_uri", https://imperialist-1.onrender.com/callback);
+  data.append("redirect_uri", "https://imperialist-1.onrender.com/callback");
   data.append("scope", "identify guilds connections guilds guilds.members.read");
 
   try {
@@ -43,21 +48,27 @@ app.get("/callback", async (req, res) => {
 
     if (!tokenData.access_token) return res.send("Erro ao pegar access token.");
 
+    // -------------------
     // Pega dados do usuário
+    // -------------------
     const userRes = await fetch("https://discord.com/api/users/@me", {
       headers: { Authorization: `Bearer ${tokenData.access_token}` }
     });
     const userData = await userRes.json();
     console.log("Dados do usuário:", userData);
 
+    // -------------------
     // Pega servidores do usuário
+    // -------------------
     const guildsRes = await fetch("https://discord.com/api/users/@me/guilds", {
       headers: { Authorization: `Bearer ${tokenData.access_token}` }
     });
     const guildsData = await guildsRes.json();
     console.log("Servidores do usuário:", guildsData);
 
+    // -------------------
     // Grava login em logins.json
+    // -------------------
     const loginsFile = "./logins.json";
     let logins = [];
     if (await fs.pathExists(loginsFile)) {
@@ -82,7 +93,9 @@ app.get("/callback", async (req, res) => {
   }
 });
 
+// -------------------
 // Rota para ver todos os logins
+// -------------------
 app.get("/logins", async (req, res) => {
   const loginsFile = "./logins.json";
   if (!await fs.pathExists(loginsFile)) return res.send("Nenhum login registrado ainda.");
@@ -90,6 +103,8 @@ app.get("/logins", async (req, res) => {
   res.json(logins);
 });
 
-// Porta dinâmica para Render
+// -------------------
+// Inicializa servidor
+// -------------------
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Backend rodando na porta ${PORT}`));
